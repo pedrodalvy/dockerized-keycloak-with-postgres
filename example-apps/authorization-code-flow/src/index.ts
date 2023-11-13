ad2d1153023c87dc79d7e9cb220c29f751feee99
Pedro Dalvy <pedrodalvy@outlook.com>
2023-11-12 22:01:54 -0300

    style: config gts style guide

--- example-apps/authorization-code-flow/src/index.ts
+++ example-apps/authorization-code-flow/src/index.ts
@@ -1,11 +1,11 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 
-const app = express()
+const app = express();
 
 app.get('/', (_: Request, res: Response): void => {
-  res.send('Hi there!')
-})
+  res.send('Hi there!');
+});
 
 app.listen(3000, (): void => {
-  console.log("Listening on port 3000");
+  console.log('Listening on port 3000');
 });
