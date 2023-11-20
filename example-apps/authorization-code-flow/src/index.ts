9650b4d922e02969ba4eb2f654fc179fa4f22727
Pedro Dalvy <pedrodalvy@outlook.com>
2023-11-20 09:25:43 -0300

    chore: create simple authorization code flow

--- example-apps/authorization-code-flow/src/index.ts
+++ example-apps/authorization-code-flow/src/index.ts
@@ -1,11 +1,45 @@
 import express, { Request, Response } from 'express';
+import { URLSearchParams } from 'node:url';
 
 const app = express();
 
-app.get('/', (_: Request, res: Response): void => {
-  res.send('Hi there!');
+app.get('/login', (_: Request, res: Response): void => {
+  const loginParams = new URLSearchParams({
+    client_id: 'xablau-client',
+    redirect_uri: 'http://localhost:3001/callback',
+    response_type: 'code',
+    scope: 'openid',
+  });
+
+  const keycloakURL = 'http://localhost:8080/realms/xablau/protocol/openid-connect/auth';
+
+  res.redirect(`${keycloakURL}?${loginParams.toString()}`);
 });
 
-app.listen(3000, (): void => {
-  console.log('Listening on port 3000');
+app.get('/callback', async (req: Request, res: Response): Promise<void> => {
+  const bodyParams = new URLSearchParams({
+    client_id: 'xablau-client',
+    redirect_uri: 'http://localhost:3001/callback',
+    grant_type: 'authorization_code',
+    code: req.query.code as string,
+  });
+
+  const keycloakURL = 'http://host.docker.internal:8080/realms/xablau/protocol/openid-connect/token';
+
+  // @ts-ignore
+  const keycloakResponse = await fetch(keycloakURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: bodyParams.toString(),
+  });
+
+  const result = await keycloakResponse.json();
+
+  res.json(result);
+});
+
+app.listen(3001, (): void => {
+  console.log('Listening on port 3001');
 });
